1fdfb7e0d51ab5f5c3f4eac266841a643a30b8df
naiar <naiar@Nai>
2023-03-21 18:52:16 +0000

    Commit inicial

---
+++ ETSCono/src/ComposicionCono/ComposicionCono.java
@@ -0,0 +1,22 @@
+package ComposicionCono;
+
+/**
+ * Clase composicion de un cono
+ *
+ * @author naiar
+ *
+ */
+public class ComposicionCono {
+	 private static void imprimir (Object datos) {
+	 System.out.println(datos);
+	 }
+	 public static void main (String[] args) {
+		 java.util.Random aleatorio = new java.util.Random();
+		 for (int i=1; i<=5; i++) {
+			 int radio = aleatorio.nextInt(9)+1;
+			 Cono cono = new Cono(0, 0, radio, i, "Azul");
+			 cono.imprimir();
+		 }
+	 }
+}
+
